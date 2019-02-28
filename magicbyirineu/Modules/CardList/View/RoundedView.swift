2de016ee55e58017826f1638dc9dc62f31ccd286
cs-andre-faruolo <andre.antonio.filho@accenture.com>
2019-02-28 09:57:30 -0300

    Adds: Final polishments to the RoundedView component

--- magicbyirineu/Modules/CardList/View/RoundedView.swift
+++ magicbyirineu/Modules/CardList/View/RoundedView.swift
@@ -31,19 +31,20 @@ class RoundedView: UIView{
         
         switch self.type {
         case .rounded:
+            
             self.layer.cornerRadius = 5
             self.backgroundColor = UIColor.clear
             self.clipsToBounds = true
+            
         case .roundedAndShaded:
+            
             self.layer.cornerRadius = 5
             self.backgroundColor = UIColor.white.withAlphaComponent(0.3)
             self.layer.shadowColor = UIColor.black.cgColor
             self.layer.shadowOpacity = 0.1
             self.layer.shadowOffset = CGSize(width: 0, height: 0)
             self.layer.shadowRadius = 1
-            self.layer.masksToBounds = false
-        default:
-            self.backgroundColor = UIColor.white
+            
         }
         
     }
