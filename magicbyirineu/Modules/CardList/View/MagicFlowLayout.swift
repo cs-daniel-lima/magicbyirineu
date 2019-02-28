b2dcea77cdd3d73b5032ebd6d825a36b80040243
Daniel Lima <daniel.da.cunha.lima@accenture.com>
2019-02-28 15:41:41 -0300

    Improve MagicFlowLayout to align elements to the left side of the UICollectionView Also adds an extension to UICollectionViewLayoutAttributes to allow the layoutAttribute frame  to have its x position modified in order make sure that the respective cell is aligned left

--- magicbyirineu/Modules/CardList/View/MagicFlowLayout.swift
+++ magicbyirineu/Modules/CardList/View/MagicFlowLayout.swift
@@ -15,18 +15,68 @@ class MagicFlowLayout: UICollectionViewFlowLayout {
         setupDefaultLayout()
     }
     
-    func setupDefaultLayout(){
-        
+    private func setupDefaultLayout(){
         minimumInteritemSpacing = 0
         minimumLineSpacing = 16
+        
         sectionHeadersPinToVisibleBounds = true
-        
         headerReferenceSize = CGSize(width: 200, height: 41)
-        
     }
     
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func layoutAttributesForElements(in rect: CGRect) -> [UICollectionViewLayoutAttributes]? {
+        
+        guard let layoutAttributesList = copyDefaultLayout(attributes: super.layoutAttributesForElements(in: rect)) else{
+            return nil
+        }
+        
+        for layoutAttributes in layoutAttributesList{
+            
+            align(layoutAttributes: layoutAttributes)
+            
+        }
+        
+        return layoutAttributesList
+        
+    }
+    
+    override open func layoutAttributesForItem(at indexPath: IndexPath) -> UICollectionViewLayoutAttributes? {
+        
+        guard let attributes = super.layoutAttributesForItem(at: indexPath)?.copy() as? UICollectionViewLayoutAttributes else{
+            return nil
+        }
+        
+        return attributes
+    }
+    
+    func getCollectionWidth()->CGFloat{
+        
+        if let width = collectionView?.frame.size.width{
+            return width
+        }else{
+            return UIScreen.main.bounds.size.width
+        }
+        
+    }
+    
+    func copyDefaultLayout(attributes: [UICollectionViewLayoutAttributes]?) -> [UICollectionViewLayoutAttributes]?{
+        return attributes?.map{ $0.copy() } as? [UICollectionViewLayoutAttributes]
+    }
+    
+    func align(layoutAttributes:UICollectionViewLayoutAttributes){
+        if(layoutAttributes.representedElementCategory == .cell){
+            
+            let currentIndexPath = layoutAttributes.indexPath
+            
+            if let currentItemAttributes = layoutAttributesForItem(at: currentIndexPath){
+                currentItemAttributes.alignLeft(usingLayout: self, collectionWidth: getCollectionWidth())
+                layoutAttributes.frame = currentItemAttributes.frame
+            }
+            
+        }
     }
     
 }
