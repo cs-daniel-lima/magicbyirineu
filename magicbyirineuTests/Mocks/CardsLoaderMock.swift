6d4b8cac8bb72be0103f0217df2babb3da7c3b2d
CS - Kaique Magno <kaique.magno.santos@accenture.com>
2019-03-13 16:23:53 -0300

    Modify the Mockup cards

--- magicbyirineuTests/Mocks/CardsLoaderMock.swift
+++ magicbyirineuTests/Mocks/CardsLoaderMock.swift
@@ -1,44 +1,34 @@
-//
-//  CardsLoaderMock.swift
-//  magicbyirineuTests
-//
-//  Created by kaique.magno.santos on 13/03/19.
-//  Copyright © 2019 DanielLima. All rights reserved.
-//
-
 import Foundation
 
 @testable import magicbyirineu
 
-final class CardsLoaderMock:CardsLoader {
-    
+final class CardsLoaderMock: CardsLoader {
     var isFetchedSuccess = true
-    
+
     override func fetchCards(with name: String?) {
         var cards = [
-            Card(name: "Card A", set: "SetCode A", setName: "Set A", imageUrl: "imageUrl A", types: ["Type A"]),
-            Card(name: "Card B", set: "SetCode A", setName: "Set A", imageUrl: "imageUrl B", types: ["Type A"]),
-            Card(name: "Card C", set: "SetCode A", setName: "Set A", imageUrl: "imageUrl C", types: ["Type A"])
+            Card(name: "Forest", set: "2ED", setName: "Unlimited Edition", imageUrl: "http://gatherer.wizards.com/Handlers/Image.ashx?multiverseid=890&type=card", types: ["Land"], foreignNames: [ForeignNames(name: "Floresta", imageUrl: "http://gatherer.wizards.com/Handlers/Image.ashx?multiverseid=890&type=card")]),
+            Card(name: "Kamahl, Pit Fighter", set: "P15A", setName: "15th Anniversary Cards", imageUrl: "http://gatherer.wizards.com/Handlers/Image.ashx?multiverseid=890&type=card", types: ["Creature"], foreignNames: nil),
+            Card(name: "Time Sieve", set: "ARB", setName: "Alara Reborn", imageUrl: nil, types: ["Artifact"], foreignNames: [ForeignNames(name: "Peneira de Tempo", imageUrl: "http://gatherer.wizards.com/Handlers/Image.ashx?multiverseid=189649&type=card")]),
+            Card(name: "Vedalken Ghoul", set: "ARB", setName: "Alara Reborn", imageUrl: nil, types: ["Creature"], foreignNames: nil),
+            Card(name: "Naga Oracle", set: "AKH", setName: "Amonkhet", imageUrl: "http://gatherer.wizards.com/Handlers/Image.ashx?multiverseid=426764&type=card", types: ["Creature"], foreignNames: nil),
         ]
-        
+
         if let name = name {
             cards = cards.filter({ (card) -> Bool in
-                return card.name.lowercased().contains(name.lowercased())
+                card.name.lowercased().contains(name.lowercased())
             })
         }
-        
-        
-        if self.isFetchedSuccess {
-            self.delegate?.loaded(
+
+        if isFetchedSuccess {
+            delegate?.loaded(
                 cards: cards,
                 forType: "A",
                 andSet: CardSet(code: "SetCode A", name: "Set A", type: "Type A", releaseDate: Date()),
                 from: self
             )
-        }else{
-            self.delegate?.loaded(error: ErrorMock())
+        } else {
+            delegate?.loaded(error: ErrorMock())
         }
     }
 }
-
-
