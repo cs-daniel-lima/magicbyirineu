628ef05be5adbed005d44dd78dcd25578e6b9d7c
CS - Kaique Magno <kaique.magno.santos@accenture.com>
2019-03-13 09:54:39 -0300

    Create HTTPHeaderCards which is class to represent the headers parameters of a request at Magic API

---
+++ magicbyirineu/Managers/API/Headers/HTTPHeaderCards.swift
@@ -0,0 +1,15 @@
+//
+//  HTTPHeaderCards.swift
+//  magicbyirineu
+//
+//  Created by kaique.magno.santos on 11/03/19.
+//  Copyright © 2019 DanielLima. All rights reserved.
+//
+
+import Foundation
+
+struct HTTPHeaderCards {
+    
+    let count:Int
+    let totalCount:Int
+}
