38bf06ba0c051ad1b5fbf0d59a025ccaf8f7b61b
CS - Kaique Magno <kaique.magno.santos@accenture.com>
2019-03-13 16:23:21 -0300

    Modify CardRepositoryMock to implement the HTTPHeaderCards

--- magicbyirineuTests/Mocks/CardRepositoryMock.swift
+++ magicbyirineuTests/Mocks/CardRepositoryMock.swift
@@ -8,7 +8,7 @@ class CardRepositoryMock: CardRepository {
 
     var isSuccessfull = true
 
-    func fetchCards(page _: Int?, name _: String?, setCode _: String?, type _: String?, orderParameter _: CardOrder?, completion: @escaping (Result<[Card]>, Int?) -> Void) {
+    func fetchCards(page _: Int?, name _: String?, setCode _: String?, type _: String?, orderParameter _: CardOrder?, completion: @escaping (Result<[Card]>, HTTPHeaderCards?) -> Void) {
         if isSuccessfull {
             completion(.success([
                 Card(name: "Forest", set: "2ED", setName: "Unlimited Edition", imageUrl: "http://gatherer.wizards.com/Handlers/Image.ashx?multiverseid=890&type=card", types: ["Land"], foreignNames: [ForeignNames(name: "Floresta", imageUrl: "http://gatherer.wizards.com/Handlers/Image.ashx?multiverseid=890&type=card")]),
@@ -16,7 +16,7 @@ class CardRepositoryMock: CardRepository {
                 Card(name: "Time Sieve", set: "ARB", setName: "Alara Reborn", imageUrl: nil, types: ["Artifact"], foreignNames: [ForeignNames(name: "Peneira de Tempo", imageUrl: "http://gatherer.wizards.com/Handlers/Image.ashx?multiverseid=189649&type=card")]),
                 Card(name: "Vedalken Ghoul", set: "ARB", setName: "Alara Reborn", imageUrl: nil, types: ["Creature"], foreignNames: nil),
                 Card(name: "Naga Oracle", set: "AKH", setName: "Amonkhet", imageUrl: "http://gatherer.wizards.com/Handlers/Image.ashx?multiverseid=426764&type=card", types: ["Creature"], foreignNames: nil),
-            ]), 5)
+            ]), HTTPHeaderCards(count: 5, totalCount: 5))
         } else {
             completion(.failure(CardRepositoryMockError.couldNotLoad), nil)
         }
