2e93d85f7c7a85b94b9a3cf6afa104417a7d4baa
CS - Kaique Magno <kaique.magno.santos@accenture.com>
2019-03-13 09:51:09 -0300

    Create CardOrder file and Reorganize the file structure

---
+++ magicbyirineu/Managers/API/Endpoint/Cards/Parameters/CardOrder.swift
@@ -0,0 +1,14 @@
+//
+//  CardOrder.swift
+//  magicbyirineu
+//
+//  Created by kaique.magno.santos on 11/03/19.
+//  Copyright © 2019 DanielLima. All rights reserved.
+//
+
+import Foundation
+
+enum CardOrder:String{
+    case name = "name"
+    case type = "type"
+}
